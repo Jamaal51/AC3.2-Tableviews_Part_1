4366e5bc1b268adc3f53485cabe7ab006a19bb59
harichandansingh <singh.harichandan@gmail.com>
2016-09-24 15:59:57 -0400

    Variable to control whether section by Genre or Century appears in table view

--- Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -21,14 +21,17 @@ class MovieTableViewController: UITableViewController {
         case twentyfirst
     }
     
+    // choose either "genre" or "century" to output different table view with corresponding rows and details
+    var genreOrCentury = "century"
+    
     internal var movieData: [Movie]?
-
+    
     internal let rawMovieData: [[String : Any]] = movies
     let cellIdentifier: String = "MovieTableViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.title = "Movies"
         self.tableView.backgroundColor = UIColor.blue
         
@@ -78,49 +81,95 @@ class MovieTableViewController: UITableViewController {
             }
         }
         
+        // after filtering, sort
         let filteredByCentury = movieData?.filter(filter).sorted {$0.year < $1.year}
         
         return filteredByCentury
     }
-
-
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        var numberOfSections = 1
+        if genreOrCentury == "genre" {
+            numberOfSections = 3
+        }
+        else if genreOrCentury == "century" {
+            numberOfSections = 2
+        }
+        return numberOfSections
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let century = Century.init(rawValue: section), let data = byCentury(century)
-            else {
-                return 0
+        var rowCount = 0
+        if genreOrCentury == "genre" {
+            guard let genre = Genre.init(rawValue: section), let data = byGenre(genre)
+                else {
+                    return 0
+            }
+            rowCount = data.count
         }
-        
-        return data.count
+        else if genreOrCentury == "century" {
+            guard let century = Century.init(rawValue: section), let data = byCentury(century)
+                else {
+                    return 0
+            }
+            rowCount = data.count
+        }
+        return rowCount
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        var movieArray = [Movie]()
         
-        guard let century = Century.init(rawValue: indexPath.section), let data = byCentury(century)
-            else {
-                return cell
+        if genreOrCentury == "genre" {
+            guard let genre = Genre.init(rawValue: indexPath.section), let data = byGenre(genre)
+                else {
+                    return cell
+            }
+            movieArray = data
+        }
+        else if genreOrCentury == "century" {
+            guard let century = Century.init(rawValue: indexPath.section), let data = byCentury(century)
+                else {
+                    return cell
+            }
+            movieArray = data
         }
         
-        cell.textLabel?.text = data[indexPath.row].title
-        cell.detailTextLabel?.text = String(data[indexPath.row].year)
+        cell.textLabel?.text = movieArray[indexPath.row].title
+        cell.detailTextLabel?.text = String(movieArray[indexPath.row].year)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        guard let century = Century.init(rawValue: section) else { return "" }
-        
-        switch century {
-        case .twentieth:
-            return "20th Century"
-        case .twentyfirst:
-            return "21st Century"
+        var stringForSection = String()
+        if genreOrCentury == "genre" {
+            guard let genre = Genre.init(rawValue: section) else { return "" }
+            
+            switch genre {
+            case .action:
+                stringForSection = "Action"
+            case .animation:
+                stringForSection = "Animation"
+            case .drama:
+                stringForSection = "Drama"
+            }
         }
+        else if genreOrCentury == "century" {
+            guard let century = Century.init(rawValue: section) else { return "" }
+            
+            switch century {
+            case .twentieth:
+                stringForSection = "20th Century"
+            case .twentyfirst:
+                stringForSection = "21st Century"
+            }
+        }
+        return stringForSection
     }
 }
+
