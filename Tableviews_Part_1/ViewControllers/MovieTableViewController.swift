57a61d9e58f5219edc2c6e9a0c8f2047697eccbc
Kareem James <kareemdjames@gmail.com>
2016-09-25 14:21:26 -0400

    Completed homework

--- Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
     
+    enum Genre: Int {
+        case animation
+        case action
+        case drama
+    }
+    
+    enum Century: Int {
+        case twentiethCentury
+        case twentyFirstCentury
+    }
+    
+    enum State {
+        case byCentury
+        case byGenre
+    }
+    
+    internal var buttonSwitch: State = .byGenre
     internal var movieData: [Movie]?
 
     internal let rawMovieData: [[String : Any]] = movies
@@ -34,21 +51,138 @@ class MovieTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        switch buttonSwitch {
+        case .byCentury:
+            return 2
+        case .byGenre:
+            return 3
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieData?.count ?? 0
+        switch buttonSwitch {
+        case .byCentury:
+            guard let century = Century.init(rawValue: section), let data = byCentury(century)
+            else {
+                return 0
+        }
+            return data.count
+        case .byGenre:
+            guard let genre = Genre.init(rawValue: section), let data = byGenre(genre)
+                else {
+                    return 0
+            }
+            return data.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        guard  let data = movieData else {
+        switch buttonSwitch {
+        case .byCentury:
+            guard let century = Century.init(rawValue: indexPath.section), let data = byCentury(century)
+                else {
+                    return cell
+            }
+            
+            cell.textLabel?.text = data[indexPath.row].title
+            cell.detailTextLabel?.text = String(data[indexPath.row].year)
+            
+            return cell
+        
+        case .byGenre:
+            guard let genre = Genre.init(rawValue: indexPath.section), let data = byGenre(genre)
+                else {
+                return cell
+            }
+        
+            cell.textLabel?.text = data[indexPath.row].title
+            cell.detailTextLabel?.text = String(data[indexPath.row].year)
+        
             return cell
         }
-        cell.textLabel?.text = movieData?[indexPath.row].title
-        cell.detailTextLabel?.text = String(data[indexPath.row].year)
-        
-        return cell
     }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch buttonSwitch {
+        case .byCentury:
+            guard let century = Century.init(rawValue: section) else { return "" }
+            switch century {
+            case .twentiethCentury:
+                return "20th Century"
+            case .twentyFirstCentury:
+                return "21st Century"
+            }
+
+        case .byGenre:
+        guard let genre = Genre.init(rawValue: section) else { return "" }
+        
+        switch genre {
+        case .action:
+            return "Action"
+        case .animation:
+            return "Animation"
+        case .drama:
+            return "Drama"
+            }
+    
+        }
+    }
+
+    func byGenre(_ genre: Genre) -> [Movie]? {
+        let filter: (Movie) -> Bool
+        switch genre {
+        case .action:
+            filter = { (a) -> Bool in
+                a.genre == "action"
+            }
+        case .animation:
+            filter = { (a) -> Bool in
+                a.genre == "animation"
+            }
+        case .drama:
+            filter = { (a) -> Bool in
+                a.genre == "drama"
+            }
+            
+        }
+        
+        // after filtering, sort
+        let filtered = movieData?.filter(filter).sorted {  $0.year < $1.year }
+        
+        return filtered
+    }
+    
+    // Filter and then sort by century
+    func byCentury(_ century: Century) -> [Movie]? {
+        let filter: (Movie) -> Bool
+        switch century {
+        case .twentiethCentury:
+            filter = { (a) -> Bool in
+                a.year <= 2000
+            }
+        case .twentyFirstCentury:
+            filter = { (a) -> Bool in
+                a.year > 2000
+            }
+        }
+        let filtered = movieData?.filter(filter).sorted { $0.year < $1.year }
+        
+        return filtered
+    }
+
+    @IBAction func switchViews(_ sender: UIButton) {
+        if buttonSwitch == .byGenre {
+            buttonSwitch = .byCentury
+            switchState.setTitle("Filter by Genre", for: .normal)
+        }
+        else {
+            buttonSwitch = .byGenre
+            switchState.setTitle("Filter by Century", for: .normal)
+        }
+        self.tableView.reloadData()
+    }
+    
+    @IBOutlet weak var switchState: UIButton!
+
 }
