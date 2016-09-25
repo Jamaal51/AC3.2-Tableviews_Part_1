b7be746925ed5e6d4948e062c59698efdc491eee
defintuition <ahspadafora@gmail.com>
2016-09-25 14:17:51 -0400

    hw

--- Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class MovieTableViewController: UITableViewController {
     
     internal var movieData: [Movie]?
-
+    
     internal let rawMovieData: [[String : Any]] = movies
     let cellIdentifier: String = "MovieTableViewCell"
     
+//    enum Genre: Int {
+//        case animation
+//        case action
+//        case drama
+//    }
+    
+    enum Century: Int {
+        case TwentyFirstCentury
+        case TwentyCentury
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.title = "Movies"
-        self.tableView.backgroundColor = UIColor.blue
+        self.tableView.backgroundColor = UIColor.white
         
         // converting from array of dictionaries
         // to an array of Movie structs
@@ -29,26 +40,64 @@ class MovieTableViewController: UITableViewController {
         }
         movieData = movieContainer
     }
-
-
+    
+    
+    func byCentury(_ century: Century) -> [Movie]? {
+        let filter: (Movie) -> Bool
+        switch century {
+        case .TwentyFirstCentury:
+            filter = { (a) -> Bool in
+                a.year > 2000
+            }
+        case .TwentyCentury:
+            filter = { (a) -> Bool in
+                a.year < 2000
+            }
+            
+        }
+        
+        // after filtering, sort
+        let filtered = movieData?.filter(filter).sorted {  $1.year < $0.year }
+        
+        return filtered
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieData?.count ?? 0
+        guard let century = Century.init(rawValue: section), let data = byCentury(century)
+            else {
+                return 0
+        }
+        return data.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        guard  let data = movieData else {
-            return cell
+        
+        guard let century = Century.init(rawValue: indexPath.section), let data = byCentury(century)
+            else {
+                return cell
         }
-        cell.textLabel?.text = movieData?[indexPath.row].title
+        
+        cell.textLabel?.text = data[indexPath.row].title
         cell.detailTextLabel?.text = String(data[indexPath.row].year)
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let century = Century.init(rawValue: section) else { return "" }
+        
+        switch century {
+        case .TwentyCentury:
+            return "20th Century"
+        case .TwentyFirstCentury:
+            return "21st Century"
+        }
     }
 }
